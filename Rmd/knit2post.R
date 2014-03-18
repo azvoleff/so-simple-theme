0f3b4765bc29d2aaf012cde954297a219db5abcd
Alex Zvoleff <azvoleff@gmail.com>
2014-03-18 16:28:37 -0400

    Update image classification post.

--- Rmd/knit2post.R
+++ Rmd/knit2post.R
@@ -1,10 +1,12 @@
-knit2post <- function(input, base.url="/_posts", fig.path="/_images") {
+knit2post <- function(input, local_base_dir='..', base.url="/") {
     require(knitr)
     require(tools)
-    opts_knit$set(base.url = base.url)
-    opts_chunk$set(fig.path=file.path(fig.path, sub(".Rmd$", "", basename(input))))
-    opts_chunk$set(fig.cap = "center")
+    opts_knit$set(base.url=base.url)
+    fig.path <- file.path(local_base_dir, 'images', sub(".Rmd$", "", basename(input)))
+    if (!file_test('-d', fig.path)) dir.create(fig.path)
+    opts_chunk$set(fig.path=paste0(fig.path, '/'))
+    opts_chunk$set(fig.cap="center")
     render_jekyll()
-    output <- file.path(base.url, paste0(file_path_sans_ext(basename(input)), '.md'))
-    knit(file.path(cur_dir, input), envir = parent.frame(), encoding="UTF-8")
+    output_md <- file.path(local_base_dir, '_posts', paste0(file_path_sans_ext(basename(input)), '.md'))
+    knit(input, output_md, envir=parent.frame())
 }
