3e0bfae0808ccb5e182e091117e2e08c8c94cdf6
Alex Zvoleff <azvoleff@gmail.com>
2014-03-18 14:17:30 -0400

    Update knitpost function.

--- Rmd/knit2post.R
+++ Rmd/knit2post.R
@@ -1,9 +1,12 @@
-KnitPost <- function(input, base.url = "/") {
+knit2post <- function(input, base.url = "/_posts") {
+    cur_dir <- getwd()
+    setwd('../_posts')
     require(knitr)
     opts_knit$set(base.url = base.url)
     fig.path <- paste0("figs/", sub(".Rmd$", "", basename(input)), "/")
     opts_chunk$set(fig.path = fig.path)
     opts_chunk$set(fig.cap = "center")
     render_jekyll()
-    knit(input, envir = parent.frame())
+    knit(file.path(cur_dir, input), envir = parent.frame())
+    setwd(cur_dir)
 }
